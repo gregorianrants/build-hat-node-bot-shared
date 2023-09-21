f1ed9b64b78f52331de130454eac2b5624db5735
gregorianrants <gregorian_rants@hotmail.com>
2023-09-21 19:27:47 +0100

    refactored publisher and subscriber to use classes and have cleaner code

---
+++ network_py/Publisher.py
@@ -0,0 +1,60 @@
+import zmq
+import json
+
+class Publisher():
+    def __init__(self,context,address,node,topic):
+        self.context = context
+        self.publicationAddress = None
+        self.socket = None
+        self.address = address
+        self.node = node
+        self.topic = topic
+        self.register()
+
+    def register(self):
+        socket = self.context.socket(zmq.REQ)
+        socket.connect("tcp://192.168.178.47:3000")
+        socket.send_json(
+            {
+                "action": "register",
+                "register_as": "publisher",
+                "topic": self.topic,
+                "node": self.node,
+                "address": self.address,
+            }
+        )
+
+        message = socket.recv_json()
+        #print(message)
+        self.publicationAddress =  message["data"]["fullAddress"]
+        self.socket= self.context.socket(zmq.PUB)
+        self.socket.bind(self.publicationAddress)
+
+    def send_json(self,py_dict):
+        as_json = json.dumps(py_dict)
+        self.socket.send_string(as_json)
+
+
+
+
+
+
+
+def registerPublisher(context,address,node,topic):
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://192.168.178.47:3000")
+    socket.send_json(
+        {
+            "action": "register",
+            "register_as": "publisher",
+            "topic": topic,
+            "node": node,
+            "address": address,
+        }
+    )
+
+    message = socket.recv_json()
+    #print(message)
+    return message["data"]["fullAddress"]
+
+
---
+++ network_py/Subscriber.py
@@ -0,0 +1,74 @@
+import zmq
+import time
+
+
+class Subscriber:
+    def __init__(self,context,subscribe_to_topic,subscribe_to_node='any'):
+        self.context = context
+        self.socket = None
+        self.publishserAddress = None
+        self.subscribe_to_node = subscribe_to_node
+        self.subscribe_to_topic = subscribe_to_topic
+        self.register()
+        self.connect()
+
+    def register(self):
+        socket = self.context.socket(zmq.REQ)
+        socket.connect("tcp://192.168.178.47:3000")
+        success = False
+
+        while not success:
+            socket.send_json(
+                {"action": "register", 
+                "register_as": "subscriber", 
+                "subscribe_to_node": self.subscribe_to_node, 
+                "subscribe_to_topic": self.subscribe_to_topic
+                }
+            )
+
+            message = socket.recv_json()
+            if message['result'] == 'success':
+                success = True
+                print('connected')
+                self.publishserAddress=message['data']['fullAddress']
+                return message['data']['fullAddress']
+            time.sleep(0.1)
+
+    def connect(self):
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect(self.publishserAddress)
+        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
+        self.socket.setsockopt(zmq.CONFLATE, 1)
+
+
+    def __next__(self):
+            received_bytes = self.socket.recv_multipart()
+            return received_bytes
+
+    def __iter__(self):
+        return self
+
+
+
+def register(context):
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://192.168.178.47:3000")
+
+    success = False
+
+    while not success:
+        socket.send_json(
+            {"action": "register", 
+             "register_as": "subscriber", 
+             "subscribe_to_node": "camera", 
+             "subscribe_to_topic": "frame"
+             }
+        )
+
+        message = socket.recv_json()
+        if message['result'] == 'success':
+            success = True
+            print('connected')
+            return message['data']['fullAddress']
+        time.sleep(0.1)
+        #print('still in loop')
