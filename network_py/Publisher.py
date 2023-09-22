969ddaa00eabe983db233385999ebb34bd78f3db
gregorianrants <gregorian_rants@hotmail.com>
2023-09-22 16:07:11 +0100

    added send_bytes to publisher

--- network_py/Publisher.py
+++ network_py/Publisher.py
@@ -1,8 +1,9 @@
 import zmq
 import json
 
-class Publisher():
-    def __init__(self,context,address,node,topic):
+
+class Publisher:
+    def __init__(self, context, address, node, topic):
         self.context = context
         self.publicationAddress = None
         self.socket = None
@@ -25,22 +26,22 @@ class Publisher():
         )
 
         message = socket.recv_json()
-        #print(message)
-        self.publicationAddress =  message["data"]["fullAddress"]
-        self.socket= self.context.socket(zmq.PUB)
+        # print(message)
+        self.publicationAddress = message["data"]["fullAddress"]
+        self.socket = self.context.socket(zmq.PUB)
         self.socket.bind(self.publicationAddress)
 
-    def send_json(self,py_dict):
+    def send_json(self, py_dict):
         as_json = json.dumps(py_dict)
         self.socket.send_string(as_json)
 
+    def send_bytes(self, data):
+        message = [bytes(self.node, "UTF-8"), bytes(self.topic, "UTF-8"), data]
+        # print(message)
+        self.socket.send_multipart(message)
 
 
-
-
-
-
-def registerPublisher(context,address,node,topic):
+def registerPublisher(context, address, node, topic):
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://192.168.178.47:3000")
     socket.send_json(
@@ -54,7 +55,5 @@ def registerPublisher(context,address,node,topic):
     )
 
     message = socket.recv_json()
-    #print(message)
+    # print(message)
     return message["data"]["fullAddress"]
-
-
--- network_py/Subscriber.py
+++ network_py/Subscriber.py
@@ -2,8 +2,10 @@ import zmq
 import time
 
 
+# TODO add a flag to change mode so that iterator can deal with different kinds of data
+# currently recieveing bytes but we also will have subscribers that recieve json.
 class Subscriber:
-    def __init__(self,context,subscribe_to_topic,subscribe_to_node='any'):
+    def __init__(self, context, subscribe_to_topic, subscribe_to_node="any"):
         self.context = context
         self.socket = None
         self.publishserAddress = None
@@ -19,35 +21,34 @@ class Subscriber:
 
         while not success:
             socket.send_json(
-                {"action": "register", 
-                "register_as": "subscriber", 
-                "subscribe_to_node": self.subscribe_to_node, 
-                "subscribe_to_topic": self.subscribe_to_topic
+                {
+                    "action": "register",
+                    "register_as": "subscriber",
+                    "subscribe_to_node": self.subscribe_to_node,
+                    "subscribe_to_topic": self.subscribe_to_topic,
                 }
             )
 
             message = socket.recv_json()
-            if message['result'] == 'success':
+            if message["result"] == "success":
                 success = True
-                print('connected')
-                self.publishserAddress=message['data']['fullAddress']
-                return message['data']['fullAddress']
+                print("connected")
+                self.publishserAddress = message["data"]["fullAddress"]
+                return message["data"]["fullAddress"]
             time.sleep(0.1)
 
     def connect(self):
         self.socket = self.context.socket(zmq.SUB)
         self.socket.connect(self.publishserAddress)
-        self.socket.setsockopt(zmq.SUBSCRIBE, b'')
+        self.socket.setsockopt(zmq.SUBSCRIBE, b"")
         self.socket.setsockopt(zmq.CONFLATE, 1)
 
-
     def __next__(self):
-            received_bytes = self.socket.recv_multipart()
-            return received_bytes
+        received_bytes = self.socket.recv_multipart()
+        return received_bytes
 
     def __iter__(self):
         return self
-
 
 
 def register(context):
@@ -58,17 +59,18 @@ def register(context):
 
     while not success:
         socket.send_json(
-            {"action": "register", 
-             "register_as": "subscriber", 
-             "subscribe_to_node": "camera", 
-             "subscribe_to_topic": "frame"
-             }
+            {
+                "action": "register",
+                "register_as": "subscriber",
+                "subscribe_to_node": "camera",
+                "subscribe_to_topic": "frame",
+            }
         )
 
         message = socket.recv_json()
-        if message['result'] == 'success':
+        if message["result"] == "success":
             success = True
-            print('connected')
-            return message['data']['fullAddress']
+            print("connected")
+            return message["data"]["fullAddress"]
         time.sleep(0.1)
-        #print('still in loop')
+        # print('still in loop')
